d93832e97baaa02ed91fe8586eec61c6bf6fda69
Shahram Ghassemi <shahramg92@hotmail.com>
2018-06-27 18:18:35 -0500

    Update

--- AlgoCasts/exercises/palindrome/index.js
+++ AlgoCasts/exercises/palindrome/index.js
@@ -13,6 +13,11 @@ function palindrome(str) {
 
 module.exports = palindrome;
 
+
+
+
+
+
 // function palindrome(str) {
 //   const reversed = str.split('').reverse().join('');
 //   return str === reversed;
