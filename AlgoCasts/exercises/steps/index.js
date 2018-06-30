9a55ef7a61d0e2d8c4d787686e2f2fd8a17d841b
Shahram Ghassemi <shahramg92@hotmail.com>
2018-06-29 19:59:43 -0500

    Update

--- AlgoCasts/exercises/steps/index.js
+++ AlgoCasts/exercises/steps/index.js
@@ -17,6 +17,32 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+
+}
 
 module.exports = steps;
+
+
+
+
+
+
+
+
+
+// function steps(n) {
+//   for (let row = 0; row < n; row ++) {
+//     let stair = '';
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//
+//     console.log(stair);
+//   }
+// }
