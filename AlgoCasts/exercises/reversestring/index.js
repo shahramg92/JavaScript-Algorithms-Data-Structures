85fdad4a0fe13dc932fbe7cb959ee2df21d2f7eb
Shahram Ghassemi <shahramg92@hotmail.com>
2018-06-30 19:35:23 -0500

    update

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -7,10 +7,15 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
 }
 
 module.exports = reverse;
+
 
 
 
