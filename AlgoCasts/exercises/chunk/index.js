d3c5661433b9202e9d6ddb805b5208c9612a0b0a
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-03 18:54:36 -0500

    update

--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -12,11 +12,6 @@ function chunk(array, size) {
   const chunked = [];
   let index = 0;
 
-  while (index < array.length) {
-    chunked.push(array.slice(index, index + size))
-    index += size;
-  }
-  return chunked;
 }
 
 module.exports = chunk;
