65f4235b50cdff71ea5dc1eb1bf339314eb87ad1
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-04 18:02:04 -0500

    update

--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -12,6 +12,12 @@ function chunk(array, size) {
   const chunked = [];
   let index = 0;
 
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
 }
 
 module.exports = chunk;
@@ -22,21 +28,9 @@ module.exports = chunk;
 
 
 
-// function chunk(array, size) {
-//   const chunked = [];
-//
-//   for (let element of array) {
-//     const last = chunked[chunked.length - 1];
-//
-//     if (!last || last.length === size) {
-//       chunked.push([element]);
-//     } else {
-//       last.push(element);
-//     }
-//   }
-//
-//   return chunked;
-// }
+function chunk(array, size) {
+
+}
 
 // function chunk(array, size) {
 //   const chunked = [];
