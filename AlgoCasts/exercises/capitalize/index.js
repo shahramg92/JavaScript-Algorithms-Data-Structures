7922c5b6846a972743e28b19b023a0d129591eca
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-07 16:08:56 -0500

    update

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -8,16 +8,19 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  const words = [];
 
-  for (let word of str.split(' ')) {
-    words.push(word[0].toUpperCase() + word.slice(1))
-  }
-
-  return words.join(' ');
 }
 
 module.exports = capitalize;
+
+
+
+
+
+
+
+
+
 
 // function capitalize(str) {
 //   const words = [];
