e0b88b6e6bc291efc99038fc9214ae170f961fda
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-11 18:19:37 -0500

    update

--- AlgoCasts/exercises/vowels/index.js
+++ AlgoCasts/exercises/vowels/index.js
@@ -8,16 +8,21 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-let count = 0;
-let checker = ['a', 'e', 'i', 'o', 'u'];
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
 
-for (let char of str.toLowerCase()) {
-  if (checker.includes(char)) {
-    count++
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++
+    }
   }
-}
   return count;
 }
+
+
+
+
+
 
 module.exports = vowels;
 
