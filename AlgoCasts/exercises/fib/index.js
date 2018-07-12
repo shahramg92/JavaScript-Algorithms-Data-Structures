577139bfdff4d973c059fb0b519944ef9a24c91a
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-12 00:34:31 -0500

    memoized recursive solution

--- AlgoCasts/exercises/fib/index.js
+++ AlgoCasts/exercises/fib/index.js
@@ -8,7 +8,25 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+// function fib(n) {
+//
+// }
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
@@ -16,6 +34,7 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 
+const fib = memoize(slowFib);
 
 module.exports = fib;
 
@@ -39,6 +58,10 @@ module.exports = fib;
 //   return result[n];
 // }
 
+
+
+
+// slowFib function
 // function fib(n) {
 //   if (n < 2) {
 //     return n;
