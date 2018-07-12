050073e3089a8ab858d87a5084f5e9847f69a521
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-12 16:54:25 -0500

    recursive fib

--- AlgoCasts/exercises/fib/index.js
+++ AlgoCasts/exercises/fib/index.js
@@ -8,33 +8,15 @@
 // Example:
 //   fib(4) === 3
 
-// function fib(n) {
-//
-// }
-
-function memoize(fn) {
-  const cache = {};
-  return function(...args) {
-    if (cache[args]) {
-      return cache[args];
-    }
-
-    const result = fn.apply(this, args);
-    cache[args] = result;
-
-    return result;
-  };
-}
-
-function slowFib(n) {
+function fib(n) {
   if (n < 2) {
     return n;
   }
 
-  return fib(n - 1) + fib(n - 2);
+  return fib(n - 1) + fib(n - 2)
 }
 
-const fib = memoize(slowFib);
+
 
 module.exports = fib;
 
@@ -69,3 +51,28 @@ module.exports = fib;
 //
 //   return fib(n - 1) + fib(n - 2);
 // }
+
+
+// function memoize(fn) {
+//   const cache = {};
+//   return function(...args) {
+//     if (cache[args]) {
+//       return cache[args];
+//     }
+//
+//     const result = fn.apply(this, args);
+//     cache[args] = result;
+//
+//     return result;
+//   };
+// }
+//
+// function slowFib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//
+//   return fib(n - 1) + fib(n - 2);
+// }
+//
+// const fib = memoize(slowFib);
