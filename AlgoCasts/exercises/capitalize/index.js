e086814bf32dce19e9054c8e4c0c57984e41aa57
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-15 20:00:46 -0500

    update

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -22,12 +22,12 @@ module.exports = capitalize;
 
 
 
-function capitalize(str) {
-  const words = [];
-
-  for (let word of str.split(' ')) {
-    words.push(word[0].toUpperCase() + word.slice(1))
-  }
-
-  return words.join(' ');
-}
+// function capitalize(str) {
+//   const words = [];
+//
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+//
+//   return words.join(' ');
+// }
