7861819bee271fc12cf29e1711ec68f90e2b06c3
Shahram Ghassemi <shahramg92@hotmail.com>
2018-07-23 16:17:13 -0500

    fizzbuzz

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -6,21 +6,6 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  let charMap = {};
-  let maxChar = '';
-  let max = 0;
-
-  for (let char of str) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-
-  for (let char in charMap) {
-    if (charMap[char] > max) {
-      max = charMap[char]
-      maxChar = char;
-    }
-    return maxChar;
-  }
 
 }
 
