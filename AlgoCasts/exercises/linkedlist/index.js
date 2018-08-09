719b5fc67cdfb561529e393495dd9ae163010b54
Shahram Ghassemi <shahramg92@hotmail.com>
2018-08-08 23:51:55 -0500

    insert last

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -79,27 +79,18 @@ class LinkedList {
     previous.next = null;
   }
 
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      // There are some existing nodes in our chain
+      last.next = new Node(data);
+    } else {
+      // The chain is empty
+      this.head = new Node(data)
+    }
+  }
+
 }
 
 module.exports = { Node, LinkedList };
-
-
-
-
-
-
-
-
-
-
-
-// Excercises!
-//
-// Ex. 1:
-//
-// class Node {
-//   constructor (data, next = null) {
-//     this.data = data;
-//     this.next = next;
-//   }
-// }
