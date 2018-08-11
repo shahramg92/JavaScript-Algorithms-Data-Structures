80197a1ec66f4b822a3b7e39cbbf1ef3e6efe2e2
Shahram Ghassemi <shahramg92@hotmail.com>
2018-08-11 00:02:47 -0500

    update

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -11,12 +11,9 @@
 
 
 function anagrams(stringA, stringB) {
-  return cleanString(stringA) === cleanString(stringB);
 
-function cleanString(str) {
-  return str.replace(/[^\w]/g, ' ').toLowerCase().split('').sort().join('');
+
 }
-
 
 
 
